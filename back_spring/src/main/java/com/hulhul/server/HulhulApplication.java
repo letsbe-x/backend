b4512f4df59abe41b12f86947677bb174fbb6ab0
Xxings <10years.shimhyeonggwan@gmail.com>
2020-05-20 17:41:51 +0900

    back_spring init

---
+++ back_spring/src/main/java/com/hulhul/server/HulhulApplication.java
@@ -0,0 +1,13 @@
+package com.hulhul.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HulhulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HulhulApplication.class, args);
+	}
+
+}
---
+++ back_spring/src/test/java/com/hulhul/server/HulhulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hulhul.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HulhulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
